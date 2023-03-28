4f639a00ab997cc209dc55029954868bd350e020
Areen Raj <areenraj@gmail.com>
2023-03-28 18:41:43 +0530

    Update montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -28,7 +28,7 @@ def calcvol(S1, k, r, ttm, optprice, type):  #r is in % and ttm is in days, take
 #We simulate the GBM through the following equation
 # delx = nu*delt + vol*sqrt(delt)*Z where x = lnS
 
-def Simulation(simlist):  #returns a results array which holds the spot price values for all time steps for all iterations and for all sims in the simlist
+def simulation(simlist):  #returns a results array which holds the spot price values for all time steps for all iterations and for all sims in the simlist
     results = []    
     for sim in simlist:
         delt = sim.T/sim.i_t
@@ -42,7 +42,7 @@ def Simulation(simlist):  #returns a results array which holds the spot price va
     return results
 
 
-def PlotMCS(results, simlist):                   #takes in the results that you get from the simulation function and plots it 
+def plotMCS(results, simlist):                   #takes in the results that you get from the simulation function and plots it 
     for i in range(len(results)):
         plt.plot(np.linspace(1, np.round(simlist[i].T*365),simlist[i].i_t+1), results[i])
         plt.title("Simulation Results")
@@ -50,7 +50,7 @@ def PlotMCS(results, simlist):                   #takes in the results that you 
         plt.ylabel("Spot Price")
         plt.show()
 
-def Payoff(results, simlist):  #takes in the list of the sim parameters and returns the price and the standard error 
+def payoff(results, simlist):  #takes in the list of the sim parameters and returns the price and the standard error 
     payoff = []
     deviation = []
     for i in range(len(simlist)): 
@@ -71,12 +71,12 @@ sim1 = Sim(101.15, 98.01, calcvol(101.15,97.01,2,61, 4.8, 'call'), 0.01, 10, 100
 sims = [sim1]   #this contains your total array of simulations, each member represents one case that is taken
 
 #Simulating
-S_t = Simulation(sims)
+S_t = simulation(sims)
 
-price, error = Payoff(S_t, sims)
+price, error = payoff(S_t, sims)
 print(price, error)
 
-PlotMCS(S_t, sims)
+plotMCS(S_t, sims)
 
 
     
